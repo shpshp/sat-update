eca50225b719a09f917c4400ddf5ca26f2a7aab5
Shp <shp@mail.ru>
2018-12-28 01:42:07 +0100

    + inf retries

--- get_all.sh
+++ get_all.sh
@@ -1,2 +1,2 @@
-cat url.txt | parallel --lb -j 8 wget -r -np "$1" --no-check-certificate -k -p --show-progress --restrict-file-names=windows
+cat url.txt | parallel --lb -j 8 wget -r -np "$1" --no-check-certificate -k -p --show-progress --restrict-file-names=windows -t 0 --retry-connrefused
 ./get_ucs-db.sh
--- get_ucs-db.sh
+++ get_ucs-db.sh
@@ -1,2 +1,2 @@
-wget -r -np -H "https://www.ucsusa.org/nuclear-weapons/space-weapons/satellite-database" --no-check-certificate -k -p --show-progress --restrict-file-names=windows -D "www.ucsusa.org,s3.amazonaws.com" -A "*.xlsx,*.txt,*" -X "sites"
+wget -r -np -H "https://www.ucsusa.org/nuclear-weapons/space-weapons/satellite-database" --no-check-certificate -k -p --show-progress --restrict-file-names=windows -D "www.ucsusa.org,s3.amazonaws.com" -A "*.xlsx,*.txt,*" -X "sites" -t 0 --retry-connrefused
 mv s3.amazonaws.com ./www.ucsusa.org/
